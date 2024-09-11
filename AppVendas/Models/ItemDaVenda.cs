d534e1ed258c25f9a586b04ae6820e4f7491913d
ÍtaloSenai2A <italooosenai@gmail.com>
2024-09-11 17:02:39 -0300

    Iniciando o projeto app vendas

---
+++ AppVendas/Models/ItemDaVenda.cs
@@ -0,0 +1,16 @@
+﻿namespace AppVendas.Models
+{
+    public class ItemDaVenda
+    {
+        /*Relacionamento com a tabela Vendas */
+        public Guid ItemDaVendaId { get; set; }
+        public Guid VendaId { get; set; }
+        public Venda? Venda { get; set; }
+
+        /* Relacionamento com a tabela Produtos */
+        public Guid ProdutoId { get; set; }
+        public Produto? Produto { get; set; }
+        public double QtadeVendida { get; set; }
+        public double ValorTotalDoItem { get; set; }
+    }
+}
